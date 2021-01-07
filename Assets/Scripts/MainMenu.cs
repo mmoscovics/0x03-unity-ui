6143f3fd68eee500f019cd6d2fc98d883466235a
Mitchell <mmoscovics@yahoo.com>
2021-01-07 17:21:11 -0500

    [Update] option menu and play

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,8 +6,30 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Material trapMat;
+    public Material goalMat;
+    public Toggle colorblindMode;
+
+    /// Loads maze scene
     public void PlayMaze()
     {
+        if (colorblindMode.isOn == true)
+        {
+            trapMat.color = new Color32(255, 112, 0, 1);
+            goalMat.color = Color.blue;
+        }
+        else
+        {
+            trapMat.color = Color.red;
+            goalMat.color = Color.green;
+        }
+
         SceneManager.LoadScene("maze");
+    }
+
+    /// "Quits" game
+    public void QuitMaze()
+    {
+        Debug.Log("Quit Game");
     }
 }
